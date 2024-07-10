a2080115c9c59c6e043aa704825bf946216a4897
Ayushman Sharma <redsteel200.as@gmail.com>
2024-07-10 12:24:03 +0530

    made the basic structure of the site no css yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from "react-router-dom"
 import './App.css';
+import RootLayout from "./Layouts/RootLayout";
+import Homepage from "./pages/Homepage";
+import Socials from "./pages/Socials";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    
+      <Route path="/" element={<RootLayout/>}>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="socials" element={<Socials/>}/>
+        <Route path="skills" element={<Skills/>}/>
+        <Route path="projects" element={<Projects/>}/>
+        
+      </Route>
+   
+  )
+)
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}/>
+    
   );
 }
 
