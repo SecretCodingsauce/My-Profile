1e1778e029b5b003a741cccb58183c2d5be03977
Ayushman Sharma <redsteel200.as@gmail.com>
2024-07-11 23:20:27 +0530

    added img files styled certification links and logos

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -5,56 +5,56 @@ import csslogo from './img/css.png'
 import gitlogo from './img/git.png'
 import githublogo from './img/github.png'
 import javascriptlogo from './img/javascript.png'
-import tailwindcsslogo from './img/tailwind-css.png' 
+import tailwindcsslogo from './img/tailwind-css.png'
 export default function Skills() {
     return (
         <div className="skills">
-            
+
             <h2>Skills</h2>
             <div className="skill-icons">
-            <div className="skill-icon">
-               
-                <p>HTML</p>
-                <img src={htmllogo}></img>
-              
-               </div>
-            <div className="skill-icon">
-               
-                <p>CSS</p>
-                <img src={csslogo}></img>
-              
-               </div>
-            <div className="skill-icon">
-               
-                <p>Javascript</p>
-                <img src={javascriptlogo}></img>
-              
-               </div>
-            <div className="skill-icon">
-               
-                <p>Tailwind CSS</p>
-                <img src={tailwindcsslogo}></img>
-              
-               </div>
-            <div className="skill-icon">
-               
-                <p>React</p>
-                <img src={reactlogo}></img>
-              
-               </div>
-            <div className="skill-icon">
-               
-                <p>Git</p>
-                <img src={gitlogo}></img>
+                <div className="skill-icon">
 
+
+                    <img className="logo" src={htmllogo}></img>
+                    <span>HTML</span>
                 </div>
-              
-               </div>
-               <div className="align-middle text-center mt-7 mb-10">
-               <NavLink className="bg-blue-800 text-white bold p-3 rounded-md"  to="certifications">My Certifications</NavLink>
-               </div>
-           <Outlet />
-        
+                <div className="skill-icon">
+
+
+                    <img className="logo" src={csslogo}></img>
+                    <span>CSS</span>
+                </div>
+                <div className="skill-icon">
+
+
+                    <img className="logo" src={javascriptlogo}></img>
+                    <span>Javascript</span>
+                </div>
+                <div className="skill-icon">
+
+
+                    <img className="logo" src={tailwindcsslogo}></img>
+                    <span>Tailwind</span>
+                </div>
+                <div className="skill-icon">
+
+
+                    <img className="logo" src={reactlogo}></img>
+                    <span>React</span>
+                </div>
+                <div className="skill-icon">
+
+
+                    <img className="logo" src={gitlogo}></img>
+                    <span>Git</span>
+                </div>
+
+            </div>
+            <div className="align-middle text-center mt-7 mb-10">
+                <NavLink className="bg-blue-800 text-white bold p-3 rounded-md" to="certifications">My Certifications</NavLink>
+            </div>
+            <Outlet />
+
         </div>
     )
 }
--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -4,20 +4,23 @@ export default function Socials(){
             <h2>Socials</h2>
           <div className="social-link">
                 <a href="https://x.com/SirAyushman888" target="_blank">
-                <p>Twitter/X</p>
-                <img src="https://th.bing.com/th?id=OIP.Xn27U1Y5z0KR-wPnCt-v0QHaG0&w=260&h=239&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></img>
+                
+                <img className="logo" src="https://th.bing.com/th?id=OIP.Xn27U1Y5z0KR-wPnCt-v0QHaG0&w=260&h=239&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></img>
+                <span>Twitter/X</span>
                </a>
                </div>
           <div className="social-link">
                 <a href="https://www.linkedin.com/in/ayushman-sharma-77687b237/">
-                <p>LinkedIn</p>
-                <img src="https://th.bing.com/th?id=OIP.T4bvEvcT3fsTRXfRD1HxpwD6D6&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></img>
+                
+                <img className="logo" src="https://th.bing.com/th?id=OIP.T4bvEvcT3fsTRXfRD1HxpwD6D6&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></img>
+                <span>linkedin</span>
                </a>
                </div>
           <div className="social-link">
                 <a href="">
-                <p>Instagram</p>
-                <img src="https://th.bing.com/th?id=OIP.jPmHyBfNRs_TTPat6HyprQHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></img>
+                
+                <img  className="logo" src="https://th.bing.com/th?id=OIP.jPmHyBfNRs_TTPat6HyprQHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></img>
+                <span>Instagram</span>
                </a>
                </div>
         </div>
