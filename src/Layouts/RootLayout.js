84379de397c5e0573d4a62cf90a67cc414f6a1d4
Ayushman <redsteel200.as@gmail.com>
2024-09-17 08:43:56 +0530

    site made fully responsive, removed dummy projects

--- src/Layouts/RootLayout.js
+++ src/Layouts/RootLayout.js
@@ -1,16 +1,33 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 export default function RootLayout (){
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Function to toggle the menu
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div>
-            <div className="p-4 mb-5  flex justify-between bg-blue-600 text-white text-xl">
+            <div className="p-4 mb-5  flex justify-between bg-blue-600 text-white text-xl ">
             <h2 className="text-2xl font-bold">My Profile</h2>
             <nav className="">
+
+            <button onClick={toggleMenu} className=" md:hidden pl-5 pr-5 " ><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg></button>
                 
+                <div className={`w-full md:w-auto md:flex ${isMenuOpen ? 'block' : 'hidden' }`}>
+                    <ul className="flex flex-col md:flex-row md:space-x-8 space-y-2 md:space-y-0">
                 <NavLink className="pl-5 pr-5 hover:font-semibold hover:underline "to="/">Home</NavLink>
                 <NavLink className="pl-5 pr-5 hover:font-semibold hover:underline"to ="skills">Skills</NavLink>
                 <NavLink className="pl-5 pr-5 hover:font-semibold hover:underline"to="projects">Projects</NavLink>
                 <NavLink className="pl-5 pr-5 hover:font-semibold hover:underline "to="socials">Socials</NavLink>
+                </ul>
+                </div>
                 
                
             </nav>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,49 +8,38 @@ export default function Projects (){
         <div className="" >
            
                
-                <a className="flex border-2 border-none  justify-center p-2 m-5 ml-14 mr-14 rounded-lg bg-slate-300 min-w-max hover:rounded-2xl hover:bg-slate-400 active:bg-slate-500  "  href="https://github.com/SecretCodingsauce" target="_blank">
-                <img className="max-w-14 max-h-14 object-contain" src={githublogo}></img>
+                <a className="flex border-2 border-none  justify-center p-2 m-5 ml-14 mr-14 rounded-lg bg-slate-300 min-w-max hover:rounded-2xl hover:bg-slate-400 active:bg-slate-500  "  href="https://github.com/SecretCodingsauce" target="_blank" rel="noreferrer">
+                <img className="max-w-14 max-h-14 object-contain" alt='' src={githublogo}></img>
                 <p className='mt-2 ps-4 text-center text-slate-600 text-3xl font-semibold'>Visit my Github</p>
                 </a>
                 
                 
             <div className='sm:grid sm:grid-cols-3 sm:justify-items-center sm:mt-10  grid grid-cols-2 justify-items-center mt-10' >
             <div className="skill-icon">
-                <a href="https://fetch-and-display-user-data.vercel.app/" target="_blank">
-                <img className="logo" src={listicon}></img>
+                <a href="https://fetch-and-display-user-data.vercel.app/" target="_blank" rel="noreferrer">
+                <img className="logo" alt='' src={listicon}></img>
                 <p className='text-xs'>Fetch and display Data</p>
                 </a>
             </div>
             <div className="skill-icon">
-                <a href="https://random-quote-fetcher.vercel.app/" target="_blank">
-                <img className="logo" src={quotesicon}></img>
+                <a href="https://random-quote-fetcher.vercel.app/" target="_blank" rel="noreferrer">
+                <img className="logo" alt='' src={quotesicon}></img>
                 <p className='text-xs'>Random Quote Generator</p>
                 </a>
             </div>
             <div className="skill-icon">
-                <a href="https://simple-calculator-beige.vercel.app/" target="_blank">
-                <img className="logo" src={calculator}></img>
+                <a href="https://simple-calculator-beige.vercel.app/" target="_blank" rel="noreferrer">
+                <img className="logo" alt='' src={calculator}></img>
                 <p className='text-xs'>Simple Calculator App</p>
                 </a>
             </div>
             <div className="skill-icon">
-                <a href="https://my-weather-app-secretcodingsauces-projects.vercel.app/" target="_blank">
-                <img className="logo" src={weathericon}></img>
+                <a href="https://my-weather-app-secretcodingsauces-projects.vercel.app/" target="_blank" rel="noreferrer" >
+                <img className="logo" alt='' src={weathericon}></img>
                 <p className='text-xs'>Simple Weather App</p>
                 </a>
             </div>
-            <div className="skill-icon">
-                <a href="" target="_blank">
-                <img className="logo" src={calculator}></img>
-                <p className='text-xs'>Simple Calculator App</p>
-                </a>
-            </div>
-            <div className="skill-icon">
-                <a href="" target="_blank">
-                <img className="logo" src={calculator}></img>
-                <p className='text-xs'>Simple Calculator App</p>
-                </a>
-            </div>
+           
             </div>
         </div>
     )
